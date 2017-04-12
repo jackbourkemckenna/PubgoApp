656012b3d70b6c1559551f66e248a87173bec747
Rostifur23 <ross.delaney23@gmail.com>
2017-04-12 20:26:27 +0100

    Restore back to commit 91ebe28

---
+++ app/src/main/java/ie/jbmnetworks/pubgo/Spalsh.java
@@ -0,0 +1,13 @@
+package ie.jbmnetworks.pubgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Spalsh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+    }
+}
